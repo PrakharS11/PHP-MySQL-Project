0d455aa044ec773edee80a479930906502f1a497
Prakhar Srivastava <prakharsahai21@gmail.com>
2022-04-10 22:19:42 +0530

    first commit

---
+++ admin_service_dashboard.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Admin Dashboard</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+      <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+      <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <!-- <link rel="stylesheet" href="style.css"> -->
+    </head>
+    <style>
+       
+       .innerright,label {
+    color: rgb(16, 170, 16);
+    font-weight:bold;
+}
+.container,
+.row,
+.imglogo {
+    margin:auto;
+}
+
+.innerdiv {
+    text-align: center;
+    /* width: 500px; */
+    margin: 0px;
+}
+input{
+    margin-left:20px;
+}
+.leftinnerdiv {
+    float: left;
+    width: 25%;
+}
+
+.rightinnerdiv {
+    float: right;
+    width: 75%;
+}
+
+.innerright {
+     background-color: rgb(25,25,112);  /* red */
+}
+
+.greenbtn {
+    background-color: rgb(244,25,112); /*blue*/
+    color: white;
+    width: 95%;
+    height: 40px;
+    margin-top: 8px;
+}
+
+.greenbtn,
+a {
+    text-decoration: none;
+    color: white;
+    font-size: large;
+}
+
+th{
+    background-color: orange;
+    color: black;
+}
+td{
+    background-color: #fed8b1;
+    color: black;
+}
+td, a{
+    color:black;
+}
+    </style>
+    <body>
+
+    <?php
+   include("data_class.php");
+
+$msg="";
+
+   if(!empty($_REQUEST['msg'])){
+    $msg=$_REQUEST['msg'];
+ }
+
+if($msg=="done"){
+    echo "<div class='alert alert-success' role='alert'>Sucssefully Done</div>";
+}
+elseif($msg=="fail"){
+    echo "<div class='alert alert-danger' role='alert'>Fail</div>";
+}
+
+    ?>
+
+
+
+        <div class="container">
+        <div class="innerdiv">
+            <div class="row"><img class="imglogo" src="images/logo22.png"/></div>
+            <div class="leftinnerdiv">
+                <Button class="greenbtn"> ADMIN</Button>
+                <Button class="greenbtn" onclick="openpart('addperson')"> ADD EMPLOYEE</Button>
+                <a href="index.php"><Button class="greenbtn" > LOGOUT</Button></a>
+            </div>
+
+            
+           
+
+
+            <div class="rightinnerdiv">   
+            <div id="addperson" class="innerright portion" style="display:none">
+            <Button class="greenbtn" >Employee Details</Button>
+            <form action="addpersonserver_page.php" method="post" enctype="multipart/form-data">
+            <label>Name:</label><input type="text" name="addname"/>
+            </br>
+            <label>Email:</label><input  type="email" name="addemail"/>
+            </br>
+            <label>Pasword:</label><input type="pasword" name="addpass"/>
+            </br>
+            <label>Salary:</label><input type="salary" name="addsalary"/>
+            </br>
+            <label for="typw">Choose type:</label>
+            <select name="type" >
+                <option value="Software Developer">Software Developer</option>
+                <option value="Senior Software Developer">Senior Software Developer</option>
+                <option value="Technical Lead">Technical Lead</option>
+                <option value="HR Coordinator">HR Coordinator</option> 
+                <option value="Technical Project Manager">Technical Project Manager</option>
+            </select>
+
+            <input type="submit" value="SUBMIT"/>
+            </form>
+            </div>
+            </div>
+
+            <div class="rightinnerdiv">   
+            <div id="studentrecord" class="innerright portion" style="display:none">
+            <Button class="greenbtn" >Student RECORD</Button>
+
+            <?php
+            $u=new data;
+            $u->setconnection();
+            $u->userdata();
+            $recordset=$u->userdata();
+
+            $table="<table style='font-family: Arial, Helvetica, sans-serif;border-collapse: collapse;width: 100%;'><tr><th style='  border: 1px solid #ddd;
+            padding: 8px;'> Name</th><th>Email</th><th>Type</th></tr>";
+            foreach($recordset as $row){
+                $table.="<tr>";
+               "<td>$row[0]</td>";
+                $table.="<td>$row[1]</td>";
+                $table.="<td>$row[2]</td>";
+                $table.="<td>$row[4]</td>";
+                // $table.="<td><a href='deleteuser.php?useriddelete=$row[0]'>Delete</a></td>";
+                $table.="</tr>";
+                // $table.=$row[0];
+            }
+            $table.="</table>";
+
+            echo $table;
+            ?>
+
+            </div>
+            </div>
+
+
+     
+        <script>
+        function openpart(portion) {
+        var i;
+        var x = document.getElementsByClassName("portion");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";  
+        }
+        document.getElementById(portion).style.display = "block";  
+        }
+        </script>
+    </body>
+</html>
